df0da8a2d9910ccb09bf5b7bb9abaa6aeb4ee964
sbocko <st.bocko@gmail.com>
2016-03-22 15:28:04 +0100

    Add REST controller.

--- src/main/java/sk/bocko/matrixcalc/Application.java
+++ src/main/java/sk/bocko/matrixcalc/Application.java
@@ -1,3 +1,6 @@
+package sk.bocko.matrixcalc;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
@@ -7,5 +10,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
