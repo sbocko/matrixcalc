312bc59a4ff6c4af009f9687c0768b166a4cd58b
sbocko <st.bocko@gmail.com>
2016-03-22 14:22:21 +0100

    Initial commit.

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by stefan on 3/22/16.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+
+}
